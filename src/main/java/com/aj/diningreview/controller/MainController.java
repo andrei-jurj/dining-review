91a05956aa59c82faf8cf6851c17890b87a70f51
Andrei Jurj <andreixjurj@gmail.com>
2022-12-22 16:07:27 +0200

    User login page

---
+++ src/main/java/com/aj/diningreview/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.aj.diningreview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/login")
+    public String viewLoginPage() {
+        return "login";
+    }
+}
